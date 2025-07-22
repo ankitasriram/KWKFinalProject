aa558e7237ff4bbab632f1302447eebbf7233258
ankitasriram <sriram40@purdue.edu>
2025-07-21 21:32:50 -0700

    fixing main.js and testing changes

--- src/sections/TitleSection.svelte
+++ src/sections/TitleSection.svelte
@@ -1,7 +1,7 @@
 <script>
     const title = "LOCKED \n OUT";
     const subtitle =
-        "Examining Barriers to Black \n Homeownership in the Bay Area and Beyond";
+        "Examining Barriers to Black \n Homeownership in the Bay Area, California, and the US";
 </script>
 
 <div class="screen">
